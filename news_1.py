6a9f965e60839a772e42d3dcd81af7bff45cc23a
goaway-bb <zakhar.khramin42@gmail.com>
2021-05-09 17:54:25 +0700

    comment 6-18 string and copy copy.html in src

--- news_1.py
+++ news_1.py
@@ -3,16 +3,18 @@ import requests
 class find_site_with_news(object):
 	def __init__(self, theme):
 		pass
-#ссылка на сайт
-url1 = "https://ria.ru/"
-#идентификация кода в виде пользователя
-headers = {
-	"Accept" : "*/*",
-	"User-Agent" : "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 YaBrowser/21.3.3.234 Yowser/2.5 Safari/537.36"
-}
-#переход на сайт с помощью кода
-req = requests.get(url1, headers=headers)
-src = req.text
-#print(src)
-with open("copy.html", "w", encoding="UTF-8") as file:
-	file.write(src)
+##ссылка на сайт
+#url1 = "https://ria.ru/"
+##идентификация кода в виде пользователя
+#headers = {
+#	"Accept" : "*/*",
+#	"User-Agent" : "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 YaBrowser/21.3.3.234 Yowser/2.5 Safari/537.36"
+#}
+##переход на сайт с помощью кода
+#req = requests.get(url1, headers=headers)
+#src = req.text
+##print(src)
+#with open("url1.html", "w", encoding="UTF-8") as file:
+#	file.write(src)
+with open("copy.html", "r") as file:
+	src = file.read()
