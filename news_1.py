290e4e8996c5c586e9b77f32d92d0e22bbf5d759
goaway-bb <zakhar.khramin42@gmail.com>
2021-05-09 17:19:10 +0700

    я решил оставить один сайт и сделать сначала програм через неё, перешёл на сайт с помощью кода

--- news_1.py
+++ news_1.py
@@ -1,19 +1,16 @@
-from bs4 import Beautifulsoup
+from bs4 import BeautifulSoup
 import requests
 class find_site_with_news(object):
 	def __init__(self, theme):
+		pass
+#ссылка на сайт
 url1 = "https://ria.ru/"
-url2 = "https://tass.ru/"
-url3 = "https://www.interfax.ru/"
-url4 = "https://www.rbc.ru/"
-url5 = "https://lenta.ru/"
-url6 = "https://www.vedomosti.ru/"
-url7 = "https://www.gazeta.ru/"
-url8 = "https://www.kommersant.ru/"
-url9 = "https://www.vesti.ru/"
-url10 = "https://echo.msk.ru/"
-url11 = "https://svpressa.ru/"
-url12 = "https://tvrain.ru/"
-url13 = "https://novayagazeta.ru/"
-url14 = "https://rg.ru/"
-url15 = "https://life.ru/"
+#идентификация кода в виде пользователя
+headers = {
+	"Accept" : "*/*",
+	"User-Agent" : "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 YaBrowser/21.3.3.234 Yowser/2.5 Safari/537.36"
+}
+#переход на сайт с помощью кода
+req = requests.get(url1, headers=headers)
+src = req.text
+#print(src)
